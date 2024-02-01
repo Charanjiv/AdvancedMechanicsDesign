bf6efe10118c6f48d2e6e6c238f8d0823773e502
Charanjiv <CSLidder@gmail.com>
2024-02-01 11:41:07 +0000

    week5session2

---
+++ Assets/ECS/EnemySpawnerComponent.cs
@@ -0,0 +1,10 @@
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct EnemySpawnerComponent : IComponentData
+{
+    public Entity prefabToSpawn;
+    public float3 spawnerPosition;
+    public float spawnRate;
+    public float timer;
+}
