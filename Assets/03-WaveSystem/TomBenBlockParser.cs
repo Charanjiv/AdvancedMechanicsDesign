2932f948802546608ab20d85b8aa83cf7b3b3ad8
Charanjiv <CSLidder@gmail.com>
2024-02-22 11:58:33 +0000

    week8 session2

--- Assets/03-WaveSystem/TomBenBlockParser.cs
+++ Assets/03-WaveSystem/TomBenBlockParser.cs
@@ -1,6 +1,8 @@
+using Codice.CM.Common;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using static PlasticPipe.PlasticProtocol.Messages.Serialization.ItemHandlerMessagesSerialization;
 
@@ -37,6 +39,8 @@ public class TomBenBlockParser : MonoBehaviour
     {
         this.state = state;
         ClearBuffer();
+        
+
     }
 
     private bool BufferHas(string token) => charBuffer.EndsWith(token);
@@ -74,6 +78,7 @@ public class TomBenBlockParser : MonoBehaviour
             }
         }
         
+
         return blocks;
     }
 
@@ -84,15 +89,19 @@ public class TomBenBlockParser : MonoBehaviour
 
         if (ReachedEnd())
             return;
-
         currentBlock.type = GetLastMatchedBlockType();
         blocks.Add(currentBlock);
+
+        //Regex Type = new Regex(@"(cluster|type|wave)", RegexOptions.IgnoreCase);
+        //Match TypeMatch = Type.Match(fileContent);
+        //currentBlock.type = TypeMatch.Value;
+
         ChangeState(ParserState.InsideBlockHeader);
     }
 
     private void ParseInsideBlock()
     {
-        while (!BufferHasAny("health", "speed", "damage") && !ReachedEnd())
+        while (!BufferHasAny("_Ben") && !ReachedEnd())
             NextChar();
 
 
@@ -100,6 +109,14 @@ public class TomBenBlockParser : MonoBehaviour
             return;
 
         currentBlock.type = GetLastMatchedBlockType();
+
+
+        //string regexPattern = "(health | speed | damage)=>(\d*)";
+        //Regex regex = new Regex(regexPattern);
+        //Match regexMatch = regex.Match(inputFile)
+        Regex Content = new Regex(@"_Tom([\w\s\W]*)_Ben", RegexOptions.IgnoreCase);
+        Match ContentMatch = Content.Match(charBuffer);
+        currentBlock.content = ContentMatch.Value;
         
         ChangeState(ParserState.OutsideBlock);
 
@@ -107,14 +124,33 @@ public class TomBenBlockParser : MonoBehaviour
 
     private void ParseInsideBlockHeader()
     {
-        while (!BufferHasAny("_Tom", "_Ben") && !ReachedEnd())
-            NextChar();
-
+        //while (!BufferHasAny("_Tom", "_Ben") && !ReachedEnd())
+        while(!BufferHasAny("_Tom") && !ReachedEnd())
+        NextChar();
 
         if (ReachedEnd())
             return;
 
-        currentBlock.type = GetLastMatchedBlockType();
+
+        Regex ID = new Regex(@"(\d)");
+        Match MatchID = ID.Match(charBuffer);
+        
+
+        Regex Name = new Regex(@"(?:\((.*)\))");
+        Match NameMatch = Name.Match(charBuffer);
+
+        int.TryParse(MatchID.Groups[1].Value, out int result);
+
+        currentBlock.name = NameMatch.Groups[1].Value;
+        currentBlock.id = result;
+        
+
+        
+
+        //if (ReachedEnd())
+        //    return;
+
+        //currentBlock.type = GetLastMatchedBlockType();
         
         ChangeState(ParserState.InsideBlockBody);
 
